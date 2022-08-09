9fa890673e339a3ad7bf5b778bf573b1afdc2767
Shachar Itzhaky <corwin.amber@gmail.com>
2022-08-10 00:57:27 +0300

    [doc] Added snapshot example + updated readme.

--- README.md
+++ README.md
@@ -3,16 +3,19 @@
 Translating Python [bytecode](https://docs.python.org/3.5/library/dis.html#python-bytecode-instructions) to Three Address Code
 
 ## Pipeline operations by modules:
-
-1. [bcode.py](bcode.py) Python code to bytecode (CPython functions)
-
-  1. Fixing minor bytecode issues
-  2. Turn into BCode
   
-2. [bcode_cfg.py](instruction_cfg.py): Generate CFG of BCode
+1. [tac.py](tac.py): Translate basic block CFG into equivalent Three Address Code (TAC)
+2. [tac_analysis.py](tac_analysis.py): Data flow analysis of TAC CFG
 
-  1. Find stack depth at each point
-  2. Build basic block CFG
-  
-3. [tac.py](tac.py): Translate basic block CFG into equivalent Three Address Code (TAC)
-4. [tac_analysis.py](tac_analysis.py): Data flow analysis of TAC CFG
+
+## Working assumptions
+
+1. There are two types of functions: constructors and heap-neutral.
+   1. Constructors: create (concretely) an isolated sub-graph and returns a root
+      to it.
+   2. Heap neutral: do not change the heap object at all. In case the function
+      returns an object, a contract (in the form of a function summary) is required
+      to describe the identity of the returned object.
+
+      Contracts may be describe as logical assersions but for economy reasons we
+      will have them be the abstract transformers themselves.
---
+++ examples/feature_selection_w_snapshot.py
@@ -0,0 +1,121 @@
+import numpy as np
+import pandas as pd
+
+
+def cost_function(X, y, theta):
+    m = y.size
+    error = np.dot(X, theta.T) - y
+    cost = 1 / (2 * m) * np.dot(error.T, error)
+    return cost, error
+
+
+def gradient_descent(X, y, theta, alpha, iters):
+    cost_array = np.zeros(iters)
+    m = y.size
+    for i in range(iters):
+        cost, error = cost_function(X, y, theta)
+        theta = theta - (alpha * (1 / m) * np.dot(X.T, error))
+        cost_array[i] = cost
+    return theta, cost_array
+
+
+def predict(X, theta):
+    result = np.zeros((len(X), 1))
+    for j in range(len(X)):
+        x = X[j]
+        total = 0
+        for x, t in zip(x, theta):
+            total += x * t
+        result[j] = total
+    return result
+
+
+def run(X, y):
+    y = np.concatenate(y)
+    X = (X - X.mean()) / X.std()
+    X = np.c_[np.ones(X.shape[0]), X]
+    alpha = 0.1
+    iterations = 10000
+    theta = np.zeros(X.shape[1])
+    initial_cost, _ = cost_function(X, y, theta)
+    theta, cost_num = gradient_descent(X, y, theta, alpha, iterations)
+    final_cost, _ = cost_function(X, y, theta)
+    return final_cost, predict(X, theta)
+
+
+def Linear_Regression(features, target, dims):
+    # preprocess features and target
+    target = np.array(target).reshape(target.shape[0], -1)
+    if features[:, dims].size > 0:
+        # define sparse features
+        sparse_features = features[:, dims]
+        if sparse_features.ndim == 1:
+            sparse_features = sparse_features.reshape(sparse_features.shape[0], 1)
+        score, predict = run(sparse_features, target)
+    else:
+        score = 0
+        predict = np.zeros(features.shape[0]).reshape(features.shape[0], -1)
+    grad = np.dot(features.T, target - predict)
+    return grad, score
+
+
+def do_work(features: np.ndarray, target: np.ndarray, k: int):
+    # define rounds
+    rounds = 0
+    rounds_ind = 0
+
+    # define new solution
+    S = np.array([], int)
+    _persist_mark(S)
+
+    for idx in range(k):
+        persist_start(idx)
+        if now_recovering():
+            idx, S = _persist_recover(snapshot)  # load snapshot and set local variables to saved state
+        _persist_mark(idx)
+
+        # define and train model
+        # preprocess current solution
+        grad, _ = Linear_Regression(features, target, np.unique(S[S >= 0]))
+        rounds += 1
+
+        # define vals
+        A = np.array(range(len(grad)))
+        point = []
+        for a in np.setdiff1d(A, S):
+            point = np.append(point, a)
+        out = [[point, len(np.setdiff1d(A, S))]]
+        out = np.array(out, dtype='object')
+        rounds_ind += np.max(out[:, -1])
+
+        # get feasible points
+        points = np.array([])
+        points = np.append(points, np.array(out[0, 0]))
+        points = points.astype('int')
+        # break if points are no longer feasible
+        if len(points) == 0:
+            break
+
+        # otherwise add maximum point to current solution
+        a = points[0]
+        for i in points:
+            if grad[i] > grad[a]:
+                a = i
+
+        if grad[a] >= 0:
+            _persist_unmark(S)
+            S = np.unique(np.append(S, a))
+            _persist_mark_shallow(S)  # for now S must be ndarray (can use pickle when committing)
+        else:
+            break
+
+        persist_commit()
+        _persist_commit(idx, S)
+    return results
+
+
+def main():
+    dataset_name = "dataset_20KB"
+    features = np.load(dataset_name + "_features.npy")
+    target = np.load(dataset_name + "_target.npy")
+    do_work(features, target, 10)
