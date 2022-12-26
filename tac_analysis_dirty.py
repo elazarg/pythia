a6a0abe75bd81cb8e4243259c69adcc702096f34
Elazar Gershuni <elazarg@gmail.com>
2022-12-26 17:46:16 +0200

    simple dirty

---
+++ tac_analysis_dirty.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from typing import TypeAlias
+import tac
+from tac_analysis_domain import Object, LOCALS, Analysis
+from tac_analysis_pointer import Graph
+
+Dirty: TypeAlias = set[Object]
+
+
+def copy_dirty(dirty: Dirty) -> Dirty:
+    return dirty.copy()
+
+
+class DirtyAnalysis(Analysis[Dirty]):
+    backward: bool = False
+
+    def name(self) -> str:
+        return "Dirty"
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def is_less_than(self, left, right) -> bool:
+        return self.join(left, right) == right
+
+    def is_equivalent(self, left, right):
+        return left == right
+
+    def copy(self, values) -> Dirty:
+        return copy_dirty(values)
+
+    def initial(self, annotations: dict[tac.Var, str]) -> Dirty:
+        return set()
+
+    def bottom(self) -> Dirty:
+        return set()
+
+    def join(self, left: Dirty, right: Dirty) -> Dirty:
+        return left | right
+
+    def transfer(self, values: Dirty, ins: tac.Tac, pointers: Graph, location: str) -> Dirty:
+        values = values.copy()
+        match ins:
+            case tac.Assign(lhs=tac.Attribute(var=tac.Var() as var) | tac.Subscript(var=tac.Var() as var)):
+                values.update(pointers[LOCALS][var])
