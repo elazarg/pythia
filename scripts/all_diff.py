fea5e337ee730aa555adfc58cc77ec03a7d4e320
Elazar Gershuni <elazarg@gmail.com>
2023-03-13 13:57:29 +0200

    all_diff.py

--- scripts/all_diff.py
+++ scripts/all_diff.py
@@ -1,6 +1,6 @@
+import argparse
 import concurrent.futures
 import os
-import sys
 from pathlib import Path
 
 CHUNK_SIZE = 64
@@ -12,9 +12,9 @@ def run_count_diff(file1, file2):
     return int(output)
 
 
-def main(dumpdir):
+def main(dumpdir, chunk_size, max_workers):
     dump_files = list(sorted(Path(dumpdir).iterdir(), key=os.path.getmtime))
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         results = executor.map(run_count_diff, dump_files[:-1], dump_files[1:])
 
         for i, count in enumerate(results):
@@ -23,9 +23,11 @@ def main(dumpdir):
 
 if __name__ == '__main__':
     # Check if folder path is provided as a command-line argument
-    if len(sys.argv) < 2:
-        print("Please provide folder path as a command-line argument")
-        sys.exit(1)
-    if len(sys.argv) == 3:
-        CHUNK_SIZE = int(sys.argv[2])
-    main(sys.argv[1])
+    parser = argparse.ArgumentParser(description="Compute count_diff for pairs of dump files")
+    parser.add_argument("dump_dir", type=str, help="Directory containing the dump files")
+    parser.add_argument("-c", "--chunk_size", type=int, default=64, help="Chunk size for comparison")
+    parser.add_argument("-w", "--max_workers", type=int, default=10, help="Maximum number of worker threads")
+    args = parser.parse_args()
+
+    CHUNK_SIZE = args.chunk_size
+    main(args.dump_dir, args.chunk_size, args.max_workers)
