13a3779942dbeab64c49e16e4925e240f8d98457
Elazar Gershuni <elazarg@gmail.com>
2023-07-12 17:19:39 +0300

    pivoter.py

--- examples/pivoter.py
+++ examples/pivoter.py
@@ -1,3 +1,5 @@
+import collections
+
 
 def new(f): return f
 
@@ -10,10 +12,10 @@ def get_world(g: dict, root_to_leaf_path: list[int]) -> set[int]:
     return world
 
 
-def run(g: dict, root: int) -> int:
-    counter = 0
+def run(g: dict, root: int) -> collections.Counter[int]:
     root_to_leaf_path = [root]
-    for _ in range(10**100):  # type: int
+    counter = collections.Counter[int]()
+    for r in range(10**100):  # type: int
         if not root_to_leaf_path:
             break
         world = get_world(g, root_to_leaf_path)
@@ -21,7 +23,8 @@ def run(g: dict, root: int) -> int:
             # no children to explore
             # found maximal clique
             # print(f"Maximal Clique: {root_to_leaf_path}")
-            counter += 1
+            counter[len(root_to_leaf_path)] += 1
+            # print(f"r: {counter}", end='\r', flush=True)
         if max(world, default=0) <= root_to_leaf_path[-1]:
             # leaf node -> print clique
             # move_to_sibling(g, root_to_leaf_path, vertices):
@@ -45,13 +48,32 @@ def run(g: dict, root: int) -> int:
     return counter
 
 
-def main():
+def test():
     g = {1: {2, 3},
          2: {1, 3},
          3: {1, 4, 2},
          4: {3, 5, 6},
          5: {4, 6},
          6: {5, 4}}
+    counter = run(g, 1)
+    print(counter)
+
+
+def parse(path: str) -> dict[int, set[int]]:
+    with open(path) as f:
+        n, m = next(f).strip().split()
+        n, m = int(n), int(m)
+        edges = [line.strip().split() for line in f]
+        edges = [(int(a), int(b)) for a, b in edges]
+    result = {}
+    for a, b in edges:
+        result.setdefault(a, set()).add(b)
+        result.setdefault(b, set()).add(a)
+    return result
+
+
+def main():
+    g = parse("data/enron.edges.small.txt")
     counter = run(g, 1)
     print(counter)
 
