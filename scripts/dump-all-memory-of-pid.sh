410c5244dd8437349a159c17d743f3b6168b499f
Elazar Gershuni <elazarg@gmail.com>
2024-08-26 11:29:06 +0300

    dump-all-memory-of-pid.sh

---
+++ scripts/dump-all-memory-of-pid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# see https://serverfault.com/a/408929/298972
+
+grep rw-p "/proc/$1/maps" \
+| sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' \
+| while read -r start stop; do \
+    gdb --batch --pid "$1" -ex \
+        "dump memory $1-$start-$stop.dump 0x$start 0x$stop"; \
+done
