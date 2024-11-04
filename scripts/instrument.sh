f4ea679d6f51cfa2b6c2383360b35e40f5ffdd31
Elazar Gershuni <elazarg@gmail.com>
2024-11-04 21:36:23 +0200

    flags and instrument.sh
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

---
+++ scripts/instrument.sh
@@ -0,0 +1,6 @@
+instrument() {
+  python instrument.py --kind=proc --no-generate $(head -1 experiment/$1/flags.txt) experiment/$1/main.py $(head -1 experiment/$1/args.txt)
+  ls -lv -s --block-size=64 criu_images/$1/*/pages-1.img
+}
+
+instrument $1
