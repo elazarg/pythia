39bf166dcfcbf75bc3517e30ab4a6b345e1c0247
Elazar Gershuni <elazarg@gmail.com>
2025-05-22 16:11:53 +0300

    minor
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

--- scripts/criu_server.sh
+++ scripts/criu_server.sh
@@ -4,4 +4,5 @@ if [[ $EUID -ne 0 ]]; then
   echo "This script must be run as root (use sudo)" >&2
   exit 1
 fi
-criu service --shell-job --address /tmp/criu_service.socket
+
+criu service --shell-job --address /tmp/criu_service.socket &
