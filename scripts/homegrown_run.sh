87e087f159ed6c4460009bf96f709c82489f58f4
Elazar Gershuni <elazarg@gmail.com>
2025-06-09 10:52:11 +0300

    fix script
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

--- scripts/homegrown_run.sh
+++ scripts/homegrown_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-python3 instrument.py --kind=proc --step=${2:-1} --no-generate experiment/$1/main.py $(head -1 experiment/$1/args.txt)
+python3 instrument.py --kind=proc --step="${2:-1}" --no-generate "experiment/$1/main.py" "$(head -1 experiment/$1/args.txt)"
