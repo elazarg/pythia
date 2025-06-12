ffb92d546447486529cff3ada1c72ef4dea5b491
Elazar Gershuni <elazarg@gmail.com>
2025-06-12 15:16:19 +0300

    homegrown_run.sh complete
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

--- scripts/homegrown_run.sh
+++ scripts/homegrown_run.sh
@@ -5,13 +5,20 @@ rm -rf "homegrown_images/$1/"
 
 python3 instrument.py --kind=proc --step="${2:-1}" --no-generate "experiment/$1/main.py" $(head -1 experiment/$1/args.txt)
 
-for f in homegrown_images/"$1"/*/parent; do scripts/homegrown_diff "$(dirname "$f")"; done
+dirs=($(ls -d homegrown_images/"$1"/*/ | sort -V))
+
+# Process in order
+for dir in "${dirs[@]}"; do
+   scripts/homegrown_diff "${dir%/}" &
+done
 
 # Wait for all background processes to finish
 wait
 
 mkdir -p "results/$1"
-cat homegrown_images/"$1"/*/diffsize.txt > "results/$1/homegrown.txt"
 
-# Remove the temporary directory
-rm -rf "homegrown_images/$1/"
+# Concatenate diffsize.txt files in numerical order
+for dir in "${dirs[@]}"; do
+   cat "${dir%/}/diffsize.txt"
+   rm "${dir%/}/"*.bin
+done > "results/$1/homegrown.txt"
