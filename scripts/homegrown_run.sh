098df54ba5f2fb562791eb8e7731fb33d46d2654
Elazar Gershuni <elazarg@gmail.com>
2025-06-12 14:42:31 +0300

    homegrown_run.sh complete
    Signed-off-by: Elazar Gershuni <elazarg@gmail.com>

--- scripts/homegrown_run.sh
+++ scripts/homegrown_run.sh
@@ -3,13 +3,13 @@ set -e
 
 python3 instrument.py --kind=proc --step="${2:-1}" --no-generate "experiment/$1/main.py" $(head -1 experiment/$1/args.txt)
 
-for f in "homegrown_images/$1/" *; do scripts/homegrown_diff "$f" & done
+for f in homegrown_images/"$1"/*/parent; do scripts/homegrown_diff "$(dirname "$f")"; done
 
 # Wait for all background processes to finish
 wait
 
 mkdir -p "results/$1"
-cat "homegrown_images/$1/*/diffsize.txt" > "results/$1/homegrown.txt"
+cat homegrown_images/"$1"/*/diffsize.txt > "results/$1/homegrown.txt"
 
 # Remove the temporary directory
 rm -rf "homegrown_images/$1/"
